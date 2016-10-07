763409a9d3674512db46679b3e6453fff68fdcfd
DavinoMJR <davinomaurojr@hotmail.com>
2016-10-07 00:53:03 -0300

    alteracao de views finalizada

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
    def after_sign_in_path_for(resource)
-    sign_in_url = new_user_session_url
+    # sign_in_url = new_user_session_url
      URI.parse('/dashboard/articles').path
     # if request.referer == sign_in_url
     #   URI.parse('/dashboard/articles').path
