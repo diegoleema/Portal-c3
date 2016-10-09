6a0c92e01d4253e1189d03e888cc2c7f2cb2d119
Davino Junior <davinomaurojr@hotmail.com>
2016-10-09 16:12:12 -0300

    alteracao views dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
    def after_sign_in_path_for(resource)
-    # sign_in_url = new_user_session_url
-     URI.parse('/dashboard/articles').path
-    # if request.referer == sign_in_url
-    #   URI.parse('/dashboard/articles').path
-    # else
-    #   stored_location_for(resource) || request.referer
-    # end
+    sign_in_url = new_user_session_url
+    if request.referer == sign_in_url # Vindo da tela de login
+      URI.parse('/dashboard/articles').path
+    else
+      sign_in_url
+      # stored_location_for(resource) || request.referer || root_path
+    end
   end
 end
--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -25,9 +25,11 @@ class Users::PasswordsController < Devise::PasswordsController
 
   # protected
 
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
+   def after_resetting_password_path_for(resource)
+     flash[:notice] = "Senha alterada com sucesso."
+     sign_out(resource)
+     super(resource)
+   end
 
   # The path used after sending reset password instructions
   # def after_sending_reset_password_instructions_path_for(resource_name)
